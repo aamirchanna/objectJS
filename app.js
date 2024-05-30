22dccd6e5ffc7d8cfab5f515f4331850a12770a9
aamir <aamirchanna127@gmail.com>
2024-05-29 23:58:56 -0700

    updated array

--- app.js
+++ app.js
@@ -1,60 +1,54 @@
 var list = document.getElementById("list");
-var fruits = [
+var products = [
   {
-    name: "Mango",
-    title: "Phalon ka badshah",
-    types: ["Chounsa", "Sindhri", "Langra", "Anwaratol", "Almaas"],
+    name: "Laptop",
+    title: "Powerful Computing",
+    types: ["Gaming Laptop", "Ultrabook", "2-in-1 Laptop"],
     desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
-    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA-IswcSxzeUy3VBIXW6hVl5RFdYqm41kFBg&s"
+    image: "https://cdn.thewirecutter.com/wp-content/media/2023/06/bestlaptops-2048px-9765.jpg?auto=webp&quality=75&width=980&dpr=1.5",
   },
   {
-    name: "Cherry",
-    title: "Khoon banane wali",
-    types: ["Black Cherry", "Red Cherry"],
-    
+    name: "Smartwatch",
+    title: "Wearable Tech",
+    types: ["Fitness Tracker", "Smartwatch", "Hybrid Smartwatch"],
     desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
-    image:
-      "https://plus.unsplash.com/premium_photo-1688671921089-628b61dae149?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlcnJ5fGVufDB8fDB8fHww",
+    image: "https://media.wired.com/photos/6511aab1189c419c40374c92/16:9/w_2400,h_1350,c_limit/Apple-Watch-Ultra-2-Alt-Gear.jpg",
   },
   {
-    name: "Orange",
-    title: "Vitaman C ki kami pori krny wala",
-    types: ["Malta", "Malta"],
-    
-    desc: "Lorem ipsum, dolor sit amet consectetur elit. ",
-    image:"https://cdn.mos.cms.futurecdn.net/xCZ2VwHz6pDUHCSVvaQsDT-1024-80.jpg.webp"
+    name: "Smartphone",
+    title: "Stay Connected",
+    types: ["Android", "iOS", "Windows"],
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
+    image: "https://media.wired.com/photos/63db2361b6ec2064d937cc48/master/pass/Have-We-Reached-Peak-Smartphone--Gadget-Lab-Gear-GettyImages-1246719267.jpg",
   },
   {
-    name: "Watermelon",
-    title: "Garmi ka dushman",
-    types: ["Red Melon", "Green Melon"],
+    name: "Tablet",
+    title: "Portable Entertainment",
+    types: ["iPad", "Android Tablet", "Windows Tablet"],
     desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
-    image:
-      "https://images.unsplash.com/photo-1621583441131-c8c190794970?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0ZXIlMjBtZWxvbnxlbnwwfHwwfHx8MA%3D%3D",
+    image: "https://p2-ofp.static.pub/fes/cms/2021/10/28/juqs65pgl1gh3dysi7yv1tnvtsiqva364946.png",
   },
   {
-    name: "Peach",
-    title: "Juicy Peach",
-    types: ["Swat Wala", "Quetta Wala"],
+    name: "Smartwatch",
+    title: "Wearable Tech",
+    types: ["Fitness Tracker", "Smartwatch", "Hybrid Smartwatch"],
     desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
-    image:
-      "https://images.unsplash.com/photo-1629828874514-c1e5103f2150?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGVhY2h8ZW58MHx8MHx8fDA%3D",
+    image: "https://sadabahaar.pk/wp-content/uploads/2023/04/5151oEvz4JL._SX522_.jpg",
   },
   {
-    name: "Cherry",
-    title: "Khoon banane wali",
-    types: ["Black Cherry", "Red Cherry"],
-    
+    name: "Headphones",
+    title: "Immersive Audio",
+    types: ["Over-Ear", "In-Ear", "On-Ear"],
     desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
-    image:
-      "https://plus.unsplash.com/premium_photo-1688671921089-628b61dae149?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlcnJ5fGVufDB8fDB8fHww",
+    image: "https://t4.ftcdn.net/jpg/03/28/37/93/360_F_328379347_xEKgEB2wkjAJmcqSTmrg4uKxfWrlL7D9.jpg",
   },
 ];
 
-function renderFruits() {
+
+function renderProduct() {
   list.innerHTML = "";
-  fruits.forEach(function (data, ind) {
-    var ele = `<div class="lg:w-1/3 sm:w-1/2 p-4">
+  products.forEach(function (data, ind) {
+    var ele = `<div class="lg:w-1/3 sm:w-1/2 p-4 border-solid rounded divide-black">
       <div class="flex relative h-[250px]">
         <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="${data.image}">
         <div id="${ind}" class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
@@ -73,15 +67,16 @@ function renderFruits() {
 
 function updateDesc(ele) {
   var index = ele.parentElement.id; 
-  fruits[index].desc = prompt("Desc");
+  products[index].desc = prompt("Desc");
+  renderProduct();
 }
 
 function deleteCard(ele) {
   var index = ele.parentElement.id; 
-  fruits.splice(index, 1); 
-  renderFruits();
+  products.splice(index, 1); 
+  renderProduct();
 }
 
-renderFruits();
+renderProduct();
 
   
